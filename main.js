74b9c491a417da5b098522c186d855381350f410
YZeitler <amz61@msn.com>
2017-09-05 16:23:44 +0300

    handlebars installed

--- main.js
+++ main.js
@@ -7,14 +7,22 @@ function fetch() {
         url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
         success: function(data) {
             console.log(data);
-            var booktitle = data.items[0].volumeInfo.title;
-            var authors = data.items[0].volumeInfo.authors;
-            var description = data.items[0].volumeInfo.description;
-            var image = data.items[0].volumeInfo.imageLinks.thumbnail;
+            $('.book-info').empty();
+            var bookmenu = {
+                booktitle: data.items[0].volumeInfo.title,
+                authors: data.items[0].volumeInfo.authors,
+                description: data.items[0].volumeInfo.description,
+                image: data.items[0].volumeInfo.imageLinks.thumbnail
+            };
+            var source = $('#store-template').html();
+            var template = Handlebars.compile(source);
+            var newHTML = template(bookmenu);
+            $('.book-info').append(newHTML);
+
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
         }
-
     });;
+
 };
