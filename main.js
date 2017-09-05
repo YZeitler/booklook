4f870ce4a66651b2224ea962e0378ec7c361bc65
YZeitler <amz61@msn.com>
2017-09-05 15:14:44 +0300

    data properties - author, description and image

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ function fetch() {
         url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
         success: function(data) {
             console.log(data);
-
+            var booktitle = data.items[0].volumeInfo.title;
+            var authors = data.items[0].volumeInfo.authors;
+            var description = data.items[0].volumeInfo.description;
+            var image = data.items[0].volumeInfo.imageLinks.thumbnail;
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
         }
+
     });;
 };
