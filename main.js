36bba22d31476af5e88575ce2a53313a90acef1c
YZeitler <amz61@msn.com>
2017-09-07 16:47:23 +0300

    looped through first 10 search items

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ $('.button').click(fetch);
 
 
 function fetch() {
+    var $isbnVal = $('#ISBN-number').val();
     $.ajax({
         method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + $isbnVal,
         success: function(data) {
             console.log(data);
             $('.book-info').empty();
-            var bookmenu = {
-                booktitle: data.items[0].volumeInfo.title,
-                authors: data.items[0].volumeInfo.authors,
-                description: data.items[0].volumeInfo.description,
-                image: data.items[0].volumeInfo.imageLinks.thumbnail
-            };
+
             var source = $('#store-template').html();
             var template = Handlebars.compile(source);
-            var newHTML = template(bookmenu);
-            $('.book-info').append(newHTML);
+
+            for (i = 0; i < data.items.length && i < 11; i++) {
+                var bookmenu = {
+                    booktitle: data.items[i].volumeInfo.title,
+                    authors: data.items[i].volumeInfo.authors,
+                    description: data.items[i].volumeInfo.description,
+                    image: data.items[i].volumeInfo.imageLinks.thumbnail
+                }
+                var newHTML = template(bookmenu);
+                $('.book-info').append(newHTML);
+            }
+
+
 
         },
         error: function(jqXHR, textStatus, errorThrown) {
